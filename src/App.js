7c5151e6beea4fb709a28c97b2d618517dd4232f
woori93 <woori93@duzon.com>
2018-11-01 13:38:30 +0900

    #5 Extra Attributes and Mixins

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import styled, {injectGlobal, keyframes} from "styled-components";
+import styled, {injectGlobal, keyframes, css} from "styled-components";
 
 injectGlobal`
     body {
@@ -15,15 +15,30 @@ class App extends Component {
                 <Button>Hello</Button>
                 <Button danger>Hello</Button>
                 <Anchor href="http://google.com">Go to Google</Anchor>
+                <Input placeholder="hello" />
             </Container>
         );
     }
 }
 
+const awesomeCard = css`
+    box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+    background-color: white;
+    border-radius: 10px;
+    padding: 20px;
+`;
+
 const Container = styled.div`
     height: 100vh;
     width: 100%;
     background-color: pink;
+`;
+
+const Input = styled.input.attrs({
+    required: true
+})`
+    border: none;
+    ${awesomeCard};
 `;
 
 const Button = styled.button`
